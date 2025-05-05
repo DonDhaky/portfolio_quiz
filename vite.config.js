39f89e409dcaa762e36c75e17188480d3f6fc06a
DonDhaky <erick.thonon@epitech.eu>
2025-05-05 05:53:00 +0200

    Edit CV et accueil 4

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  base: './', // Utilise des chemins relatifs
+  base: '/portfolio_quiz/', // Chemin de base pour GitHub Pages
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
