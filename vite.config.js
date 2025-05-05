04ce8400a53d8a27702ef0b585fbbc374dab6fac
DonDhaky <erick.thonon@epitech.eu>
2025-05-05 04:22:07 +0200

    Restore correct base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  base: '/portfolio_quiz/', // Correspond au nom de votre dépôt GitHub
+  base: '/portfolio_quiz/', // Nécessaire pour GitHub Pages
 })
