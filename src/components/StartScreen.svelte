3cea21b8a28c6712e018e01c087bc2e2d0e847f0
DonDhaky <erick.thonon@epitech.eu>
2025-05-08 02:43:19 +0200

    Add new quizz 8

--- src/components/StartScreen.svelte
+++ src/components/StartScreen.svelte
@@ -44,7 +44,7 @@
 
     <div class="quiz-container">
       <h1>L'évolution des nouvelles technologies</h1>
-      <p class="instructions">"Oh non, l'IA c'est nul, ça rend les devs moyens, ils ne connaissent plus leur métier, c'est de la triche, les tests techniques ya rien de plus pertinent !".......</p>
+      <p class="instructions">"Oh non, l'IA c'est nul, un dev qui utilise l'IA n'est pas un dev de qualité..."</p>
       <button class="start-button" on:click={() => startQuiz('tech')}>
         Démarrer le Quiz
       </button>
